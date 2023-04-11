d62dd5c674f3b4e252d2cb3dbd4b6068ec6f6682
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-04-11 14:29:18 +0000

    new changes

---
+++ program.py
@@ -0,0 +1,6 @@
+pizza_sizes= ["large", "extra-large"]
+pizza_toppings= ["pepperoni", "mushroom", "saugsage", "peppers"]
+print("Welcome, please choose the size of the pizza:")
+print(input(pizza_sizes))
+if input()!= pizza_sizes:
+    print("Error, this size of pizza is not avaible or spelling error, please try again")
