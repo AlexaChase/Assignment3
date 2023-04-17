124c23ce51a70a9d17a5cab39336bd419a0c0f5f
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-04-17 01:15:56 +0000

    new changes

--- program.py
+++ program.py
@@ -5,55 +5,55 @@ if pizzasize== "small":
 if pizzasize== "medium":
     print("Error, this size is not available")
 if pizzasize=="large":
-    bill =+ 6
+    pizzabill =+ 6
 if pizzasize=="extra-large":
-    bill=+ 10
+    pizzabill=+ 10
 topping1=input("Would you like a topping Y or N: ")
 if topping1 == "N":
-    subtotal= bill
+    subtotal= pizzabill
     print("Subtotal: $",subtotal)
-    tax= bill * 0.13
+    tax= pizzabill * 0.13
     print("Tax: $", tax)
-    finalcost= bill * 1.13
+    finalcost= pizzabill * 1.13
     print("Final Cost: $", finalcost)
 if topping1== "Y":
-    bill=+ 1
+    toppingsbill1= 1
     print(input("Please select a topping: "))
     topping2=input("Would you like a second topping Y or N: ")
     if topping2== "N":
-        subtotal= bill
+        subtotal= pizzabill + toppingsbill1
         print("Subtotal: $",subtotal)
-        tax= bill * 0.13
+        tax= (pizzabill + toppingsbill1) * 0.13
         print("Tax: $", tax)
-        finalcost= bill * 1.13
+        finalcost= (pizzabill + toppingsbill1) * 1.13
         print("Final Cost: $", finalcost)
     if topping2== "Y":
-        bill=+ 1.75
+        toppingsbill2= 1.75
         print(input("Please select a topping: "))
         topping3=input("Would you like a third topping Y or N: ")
         if topping3== "N":
-            subtotal= bill
+            subtotal= pizzabill + toppingsbill1 + toppingsbill2
             print("Subtotal: $",subtotal)
-            tax= bill * 0.13
+            tax= (pizzabill + toppingsbill1 + toppingsbill2) * 0.13
             print("Tax: $", tax)
-            finalcost= bill * 1.13
+            finalcost= (pizzabill + toppingsbill1 + toppingsbill2) * 1.13
             print("Final Cost: $", finalcost)
         if topping3== "Y":
-            bill=+ 2.50
+            toppingsbill3= 2.50
             print(input("Please select a topping: "))
             topping4=input("Would you like a third topping Y or N: ")
             if topping4== "N":
-                subtotal= bill
+                subtotal= pizzabill + toppingsbill1 + toppingsbill2 + toppingsbill3
                 print("Subtotal: $",subtotal)
-                tax= bill * 0.13
+                tax= (pizzabill + toppingsbill1 + toppingsbill2 + toppingsbill3) * 0.13
                 print("Tax: $", tax)
-                finalcost= bill * 1.13
+                finalcost= (pizzabill + toppingsbill1 + toppingsbill2 + toppingsbill3) * 1.13
                 print("Final Cost: $", finalcost)
             if topping4== "Y":
-                bill=+ 3.35
+                toppingsbill4=+ 3.35
                 print(input("Please select a topping: "))
-                subtotal= bill
+                subtotal= pizzabill + toppingsbill1 + toppingsbill2 + toppingsbill3 + toppingsbill4
                 print("Subtotal: $",subtotal)
-                tax= bill * 0.13
+                tax= (pizzabill + toppingsbill1 + toppingsbill2 + toppingsbill3 + toppingsbill4) * 0.13
                 print("Tax: $", tax)
-                finalcost= bill * 1.13
+                finalcost= (pizzabill + toppingsbill1 + toppingsbill2 + toppingsbill3 + toppingsbill4) * 1.13
